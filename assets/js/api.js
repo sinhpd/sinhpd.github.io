54e55abd466634676d497241d5f9af597a8e14c5
SinhPD <sinh.phamduy@comartek.vn>
2021-08-10 17:07:28 +0700

    Call api contact

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,29 +1,4 @@
-function initClient() {
-    var API_KEY = 'AIzaSyAx-JWbVmOfb6oLcQyqGToJvnl9588I2q8';
-    var CLIENT_ID = '807541135961-lh6t4a3t4ka6haflo3ee9jgi4ufvrd9n.apps.googleusercontent.com';
-    var SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
-
-    gapi.client.init({
-      'apiKey': API_KEY,
-      'clientId': CLIENT_ID,
-      'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(function() {
-      /*gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-      updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());*/
-    });
-  }
-
-  function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-  }
-
-  /*function updateSignInStatus(isSignedIn) {
-    if (isSignedIn) {
-        makeApiSaveContact();
-    }
-  } */
-
-  function onSubmitContact() {
+ function onSubmitContact() {
     var btn = document.getElementById("contact__from_submit");
 
     if(btn.classList.contains("processing")){
@@ -31,55 +6,43 @@ function initClient() {
     }
     btn.classList.add("processing");
     makeApiSaveContact();
-    /* var isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
-    if(!isSignedIn){
-        gapi.auth2.getAuthInstance().signIn();
-    }else{
-        makeApiSaveContact();
-    } */
   }
 
   function makeApiSaveContact() {
-    var params = {
-      // The ID of the spreadsheet to update.
-      spreadsheetId: '14RiH0Ie-LixOjGfoMhmECC-CRpnj2fCgEKh8tX9DHeY',  // TODO: Update placeholder value.
-
-      // The A1 notation of a range to search for a logical table of data.
-      // Values will be appended after the last row of the table.
-      range: 'Contacts!B:D',  // TODO: Update placeholder value.
-
-      // How the input data should be interpreted.
-      valueInputOption: 'USER_ENTERED',  // TODO: Update placeholder value.
-
-      // How the input data should be inserted.
-      insertDataOption: 'RAW',  // TODO: Update placeholder value.
-    };
-
     var name  = document.getElementById("contact-input-name").value;
     var email = document.getElementById("contact-input-email").value;
     var message = document.getElementById("contact-input-message").value;
 
-    var valueRangeBody = {
-        values: [
-            [
-            name,
-            email,
-            message
-        ]
-        ]
+    var model = {
+      name : name,
+      email : email,
+      body : message
     };
 
-    var request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
-    request.then(function(response) {
-        alert('We have received your information. Thank you very much!');
-        // Reset values input
-        document.getElementById("contact-input-name").value = "";
-        document.getElementById("contact-input-email").value = "";
-        document.getElementById("contact-input-message").value = "";
-        document.getElementById("contact__from_submit").classList.remove("processing");
-    }, function(reason) {
-        alert('We have received your information. Thank you very much!');
-        document.getElementById("contact__from_submit").classList.remove("processing");
-        console.error('error: ' + reason.result.error.message);
-    });
+    var xhr = new XMLHttpRequest();
+    var url = "https://sinhpd.herokuapp.com/api/v1/add-contact";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+
+      if(xhr.readyState === XMLHttpRequest.DONE) {
+        var status = xhr.status;
+        if (status === 0 || (status >= 200 && status < 400)) {
+          // The request has been completed successfully
+          alert('We have received your information. Thank you very much!');
+             // Reset values input
+          document.getElementById("contact-input-name").value = "";
+          document.getElementById("contact-input-email").value = "";
+          document.getElementById("contact-input-message").value = "";
+          document.getElementById("contact__from_submit").classList.remove("processing");
+        } else {
+          // Oh no! There has been an error with the request!
+          alert('An error has occurred. Please try again!');
+          document.getElementById("contact__from_submit").classList.remove("processing");
+        }
+      }
+    };
+    var data = JSON.stringify(model);
+    xhr.send(data);
+
   }
